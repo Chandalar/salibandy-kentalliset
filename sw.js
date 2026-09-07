f6b4938325e09b4531812312862e5c5c974148e5
Chandalar <mika.aaltonen@gmail.com>
2026-09-07 19:42:58 +0300

    Simple-tila: rajattu vain kenttiin 1-4, lisatty 2x YV ja 2x AV seka 2x 6vs5 ilman maalivahtia

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Fast, ultra-lightweight, 100% offline-ready & local-first
    ============================================================ */
 
-const CACHE_NAME = 'kentalliset-v51.0';
+const CACHE_NAME = 'kentalliset-v52.0';
 const APP_SHELL = [
     './',
     './index.html',
